27164b0f3f40966afe024ea79692ab810a3777c0
mayaaraquil <mayaaraquil775@gmail.com>
2023-11-20 19:26:51 -0500

    mayasPortion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { RestaurantFavesService } from './services/restaurant-faves.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderHistoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
